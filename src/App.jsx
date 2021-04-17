d170d01f6f200db68bbf2c7258efa0cbeeb3a6ee
Ghulam Mustafa <mustafaji@gmail.com>
2021-04-18 00:09:08 +0200

    updated App

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,14 @@
 import React, { Component } from "react";
-class NavBar extends Component {
+import NavBar from "./NavBar";
+import MainContent from "./MainContent";
+
+export default class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
-          <a className="navbar-brand" href="#">
-            My App
-      </a>
-
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Home
-            </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <NavBar />
+        <MainContent />
       </React.Fragment>
     );
   }
 }
-
-export default NavBar;
