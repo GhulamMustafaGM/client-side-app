6125f38cffd7a126a8ba7c8084b0ffe6efa7375c
Ghulam Mustafa <mustafaji@gmail.com>
2021-04-18 19:40:13 +0200

    created render methods

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -26,7 +26,7 @@ export default class MainContent extends Component {
     render() {
         return (
             <div>
-                <h4 className="border-bottom m-1 p-1">
+                <h4 className="m-1 p-1">
                     {this.state.pageTitle}
 
                     <span className="badge badge-secondary m-2">
@@ -35,7 +35,7 @@ export default class MainContent extends Component {
 
                     <button className="btn btn-info" onClick={this.onRefreshClick}>
                         Refresh
-          </button>
+                    </button>
                 </h4>
 
                 <table className="table">
@@ -47,24 +47,7 @@ export default class MainContent extends Component {
                             <th>City</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        {this.state.customers.map((cust) => {
-                            return (
-                                <tr key={cust.id}>
-                                    <td>{cust.id}</td>
-                                    <td>{cust.name}</td>
-                                    <td>
-                                        {cust.phone ? (
-                                            cust.phone
-                                        ) : (
-                                            <div className="bg-warning p-2 text-center">No Phone</div>
-                                        )}
-                                    </td>
-                                    <td>{cust.address.city}</td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
+                    <tbody>{this.getCustomerRow()}</tbody>
                 </table>
             </div>
         );
@@ -74,5 +57,25 @@ export default class MainContent extends Component {
     onRefreshClick = () => {
         //Update the state using setState method - so that react updates the Browser DOM automatically
         this.setState({ customersCount: 7 });
+    };
+
+    getPhoneToRender = (phone) => {
+        if (phone) return phone;
+        else {
+            return <div className="bg-warning p-2 text-center">No Phone</div>;
+        }
+    };
+
+    getCustomerRow = () => {
+        return this.state.customers.map((cust) => {
+            return (
+                <tr key={cust.id}>
+                    <td>{cust.id}</td>
+                    <td>{cust.name}</td>
+                    <td>{this.getPhoneToRender(cust.phone)}</td>
+                    <td>{cust.address.city}</td>
+                </tr>
+            );
+        });
     };
 }
