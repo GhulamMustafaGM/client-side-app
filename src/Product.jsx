7465bcd801a912d1d0c03cc6a4bec2b8846f2bd8
Ghulam Mustafa <mustafaji@gmail.com>
2021-04-20 22:24:38 +0200

    updated Product

--- src/Product.jsx
+++ src/Product.jsx
@@ -20,6 +20,9 @@ export default class Product extends Component {
 
                         <div>$ {this.state.product.price}</div>
                     </div>
+                    {/* card body ends here */}
+
+                    <div className="card-footer text-right">{this.props.children}</div>
                 </div>
             </div>
         );
