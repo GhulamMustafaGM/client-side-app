929928b47685b91c0bba211236fde70b1599841f
Ghulam Mustafa <mustafaji@gmail.com>
2021-04-22 23:47:45 +0200

    updated ShoppingCart

--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -41,22 +41,24 @@ export default class ShoppingCart extends Component {
     // render ends here
 
     //Executes after constructor and render method (includes life cycle of child components, if any) of current component
-    componentDidMount() {
-        //fetch data from data source
-
-        var promise = fetch("http://localhost:5000/products", { method: "GET" });
-        promise.then((response) => {
-            console.log(response);
-
-            var promise2 = response.json();
-            promise2.then((prods) => {
-                console.log(prods);
-
-                this.setState({ products: prods });
-            });
+    componentDidMount = async () => {
+        //send request to server
+        var response = await fetch("http://localhost:5000/products", {
+            method: "GET",
         });
+
+        //the following code executes after receiving response from server
+        //converting the response body into a JS object array
+        var prods = await response.json();
+
+        //the following code executes after converting response body into JS object array
+        console.log(prods);
+
+        //updating products into component's state
+        this.setState({ products: prods });
+
         //console.log("componentDidMount - ShoppingCart");
-    }
+    };
 
     componentDidUpdate(prevProps, prevState) {
         /*console.log(
