79d7355507b10b27bfb1be0acd1e8d9d0faecd89
Ghulam Mustafa <mustafaji@gmail.com>
2021-04-22 23:40:22 +0200

    updated ShoppingCart

--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -4,24 +4,17 @@ import Product from "./Product";
 export default class ShoppingCart extends Component {
     //Executes when the component is mounted
     constructor(props) {
-        console.log("constructor - ShoppingCart");
+        //console.log("constructor - ShoppingCart");
         super(props); //calling super class's constructor
 
         //initialization of the state
         this.state = {
-            products: [
-                { id: 1, productName: "iPhone", price: 8900, quantity: 0 },
-                // { id: 2, productName: "Sony Camera", price: 4500, quantity: 0 },
-                // { id: 3, productName: "Samsung QLED TV", price: 7745, quantity: 0 },
-                // { id: 4, productName: "iPad Pro", price: 12400, quantity: 0 },
-                // { id: 5, productName: "Xbox", price: 7780, quantity: 0 },
-                // { id: 6, productName: "Dell Monitor", price: 880, quantity: 0 },
-            ],
+            products: [],
         };
     }
 
     render() {
-        console.log("render - ShoppingCart");
+        //console.log("render - ShoppingCart");
 
         return (
             <div className="container-fluid">
@@ -50,18 +43,29 @@ export default class ShoppingCart extends Component {
     //Executes after constructor and render method (includes life cycle of child components, if any) of current component
     componentDidMount() {
         //fetch data from data source
-        console.log("componentDidMount - ShoppingCart");
+
+        var promise = fetch("http://localhost:5000/products", { method: "GET" });
+        promise.then((response) => {
+            console.log(response);
+
+            var promise2 = response.json();
+            promise2.then((prods) => {
+                console.log(prods);
+
+                this.setState({ products: prods });
+            });
+        });
+        //console.log("componentDidMount - ShoppingCart");
     }
 
     componentDidUpdate(prevProps, prevState) {
-        console.log(
-            "componentDidUpdate - ShoppingCart",
-            prevProps,
-            prevState,
-            this.props,
-            this.state
-        );
-
+        /*console.log(
+          "componentDidUpdate - ShoppingCart",
+          prevProps,
+          prevState,
+          this.props,
+          this.state
+        );*/
         // if (prevProps.x != this.props.x) {
         //   //make http call
         // }
@@ -69,12 +73,12 @@ export default class ShoppingCart extends Component {
 
     //Executes when the current instance of current component is being deleted from memory
     componentWillUnmount() {
-        console.log("componentWillUnmount - ShoppingCart");
+        //console.log("componentWillUnmount - ShoppingCart");
     }
 
     componentDidCatch(error, info) {
-        console.log("componentDidCatch - ShoppingCart");
-        console.log(error, info);
+        //console.log("componentDidCatch - ShoppingCart");
+        //console.log(error, info);
 
         localStorage.lastError = `${error}\n${JSON.stringify(info)}`;
     }
