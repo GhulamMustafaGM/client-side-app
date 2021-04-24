67213ee4257ae8efcbf292397405aa4d227c42c4
Ghulam Mustafa <mustafaji@gmail.com>
2021-04-24 22:48:41 +0200

    updated App

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,15 @@ export default class App extends Component {
     return (
       <BrowserRouter>
         <NavBar />
-        <Switch>
-          <Route path="/" exact component={Login} />
-          <Route path="/dashboard" exact component={Dashboard} />
-          <Route path="/customers" exact component={CustomersList} />
-          <Route path="/cart" exact component={ShoppingCart} />
-          <Route path="*" component={NoMatchPage} />
-        </Switch>
+        <div className="container-fluid">
+          <Switch>
+            <Route path="/" exact component={Login} />
+            <Route path="/dashboard" exact component={Dashboard} />
+            <Route path="/customers" exact component={CustomersList} />
+            <Route path="/cart" exact component={ShoppingCart} />
+            <Route path="*" component={NoMatchPage} />
+          </Switch>
+        </div>
       </BrowserRouter>
     );
   }
