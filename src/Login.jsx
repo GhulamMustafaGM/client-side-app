78dace56b227e8111bd54f4f1f4055e918ee7d2f
Ghulam Mustafa <mustafaji@gmail.com>
2021-04-26 00:46:47 +0200

    update NavBar

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,93 +1,117 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
-export default class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { email: "scott@test.com", password: "scott123", message: "" };
-    }
-
+class NavBar extends Component {
     render() {
         return (
-            <div className="row">
-                <div className="col-lg-6 mx-auto">
-                    <h4 className="my-1 py-2 border-bottom">Login</h4>
+            <React.Fragment>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
+                    <a className="navbar-brand" href="/#">
+                        eCommerce
+          </a>
 
-                    {/* Email starts */}
-                    <div className="form-group form-row">
-                        <label className="col-lg-4">Email:</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            value={this.state.email}
-                            onChange={(event) => {
-                                this.setState({ email: event.target.value });
-                            }}
-                        />
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            {!this.props.isLoggedIn ? (
+                                <li className="nav-item">
+                                    <NavLink
+                                        to="/"
+                                        className="nav-link"
+                                        activeClassName="active"
+                                        exact={true}
+                                    >
+                                        Login
+                  </NavLink>
+                                </li>
+                            ) : (
+                                ""
+                            )}
+
+                            {this.props.isLoggedIn ? (
+                                <li className="nav-item">
+                                    <NavLink
+                                        to="/dashboard"
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >
+                                        Dashboard
+                  </NavLink>
+                                </li>
+                            ) : (
+                                ""
+                            )}
+
+                            {this.props.isLoggedIn ? (
+                                <li className="nav-item">
+                                    <NavLink
+                                        to="/customers"
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >
+                                        Customers
+                  </NavLink>
+                                </li>
+                            ) : (
+                                ""
+                            )}
+
+                            {this.props.isLoggedIn ? (
+                                <li className="nav-item">
+                                    <NavLink
+                                        to="/cart"
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >
+                                        Shopping Cart
+                  </NavLink>
+                                </li>
+                            ) : (
+                                ""
+                            )}
+
+                            {this.props.isLoggedIn ? (
+                                <li className="nav-item">
+                                    <a
+                                        href="/#"
+                                        className="nav-link"
+                                        onClick={this.onLogoutClick}
+                                    >
+                                        Logout
+                  </a>
+                                </li>
+                            ) : (
+                                ""
+                            )}
+                        </ul>
                     </div>
-                    {/* Email ends */}
-
-                    {/* Password starts */}
-                    <div className="form-group form-row">
-                        <label className="col-lg-4">Password:</label>
-                        <input
-                            type="password"
-                            className="form-control"
-                            value={this.state.password}
-                            onChange={(event) => {
-                                this.setState({ password: event.target.value });
-                            }}
-                        />
-                    </div>
-                    {/* Password ends */}
-
-                    <div className="text-right">
-                        {this.state.message}
-
-                        <button className="btn btn-primary m-1" onClick={this.onLoginClick}>
-                            Login
-            </button>
-                    </div>
-                </div>
-            </div>
+                </nav>
+            </React.Fragment>
         );
-    } //end of render
-
-    componentDidMount() {
-        document.title = "Login - eCommerce";
     }
 
-    //Executes when the user clicks on Login
-    onLoginClick = async () => {
-        console.log(this.state);
+    //Executes when the user clicks on the Logout link
+    onLogoutClick = (event) => {
+        //prevent the page refresh when the user clicks on Logout link
+        event.preventDefault();
 
-        var response = await fetch(
-            `http://localhost:5000/users?email=${this.state.email}&password=${this.state.password}`,
-            { method: "GET" }
-        );
+        //update isLoggedInStatus as false
+        this.props.updateIsLoggedInStatus(false);
 
-        var body = await response.json();
-        console.log(body);
-
-        if (body.length > 0) {
-            //success
-
-            //update the message property of state of current component
-            this.setState({
-                message: <span className="text-success">Successfully Logged-in</span>,
-            });
-
-            //call the AppComponent's updateIsLoggedInStatus method
-            this.props.updateIsLoggedInStatus(true);
-
-            //navigate to dashboard
-            this.props.history.replace("/dashboard");
-        } else {
-            //error
-            this.setState({
-                message: (
-                    <span className="text-danger">Invalid login, please try again</span>
-                ),
-            });
-        }
+        //navigate login component
+        document.location.hash = "/";
     };
 }
+
+export default NavBar;
